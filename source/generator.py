cf081c6b33998dc23ee1ece304621520aa2fb76e
Jimmy <myjimmy@protonmail.com>
2024-06-27 12:32:11 -0400

    MP3: Fixed the "TypeError: 'Transcription' object is not subscriptable" error.

--- source/generator.py
+++ source/generator.py
@@ -8,7 +8,7 @@ def transcribe_audio(audio_file_path):
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file)
-    return transcription['text']
+    return transcription.text
 
 def meeting_minutes(transcription):
     abstract_summary = abstract_summary_extraction(transcription)
